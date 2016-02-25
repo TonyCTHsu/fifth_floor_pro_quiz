45ea1420d63453eff69c34497906ef0c8402e23e
Tony Hsu <tonyc.t.hsu@gmail.com>
2016-02-25 22:28:33 +0800

    spec fix sites_controller, base_controller, authorizations_controller

--- spec/controllers/base_controller_spec.rb
+++ spec/controllers/base_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BaseController, type: :request do
   it "GET /" do
-    get "/"
+    get "http://#{Setting.host}"
     expect(response).to be_success
     expect(response_meta_title).to be_present
   end
--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -14,15 +14,43 @@
 require 'rails_helper'
 
 RSpec.describe SitesController, type: :request do
-  let(:site) { FactoryGirl.create :site }
+  let(:site) { FactoryGirl.create :site, :host =>"example.com" }
 
-  describe "#show" do
-    before { get "/sites/#{site.id}" }
+  context "#show" do
+    before { get "http://#{Setting.host}/sites/#{site.id}" }
     it { expect(response).to be_success }
   end
 
-  describe "#edit" do
-    before { get "/sites/#{site.id}/edit" }
+  context "#show through subdomain" do
+    before { get "http://#{site.subdomain}.#{Setting.host}" }
     it { expect(response).to be_success }
   end
+
+  context "#show through customized domain" do
+    it "assigns site" do
+      host!(site.host)
+      get "/"
+      expect(assigns(:site).id).to eq(site.id)
+    end
+  end
+
+  context "#edit" do
+    before { get "http://#{Setting.host}/sites/#{site.id}/edit" }
+    it { expect(response).to be_success }
+  end
+
+  context "#edit through subdomain" do
+    before { get "http://#{site.subdomain}.#{Setting.host}/edit" }
+    it { expect(response).to be_success }
+  end
+
+  context "#edit through customized domain" do
+    it "assigns site" do
+      host!(site.host)
+      get "/edit"
+      expect(assigns(:site).id).to eq(site.id)
+    end
+  end
+
+
 end
